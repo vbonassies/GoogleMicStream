71f0a65501a7f8262485b1ef3448f7ea96d76925
valentin bonassies <valentin.bonassies@gmail.com>
2020-01-10 17:10:18 +0100

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+//        System.out.println(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
+        MicStream.streamingMicRecognize();
+    }
+}
